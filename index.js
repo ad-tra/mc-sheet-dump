b0563d923e9c180879ca759232ec86daa81b98ee
ad-tra <you@example.com>
2022-05-12 21:34:36 -0400

    added package.json

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const nodeHtmlToImage = require('node-html-to-image')
-let data = require('./sheetDump.json')
-let counter = 0;
-data = data.map(el => ({...el, output: `./images/${++counter}.png`}))
-console.log(data);
-
+let data = require('./sheetDump.json').map((el,i) => ({...el, output: `./images/${i}.png`}))
 
 nodeHtmlToImage({
     output: './image.png',
-    html: '<html style=" background: white; "><body style=" display: flex; align-items: center; justify-content: center; height: 823px; width: 823px; background: black; /* padding: 95px; */ "><p style=" color: #cccccc; font-family: SF Pro Display Regular; font-size: 35px; padding: 0 43px; letter-spacing: 0.5px; word-spacing: 1px; line-height: 50px; ">{{text}}</p></body></html>',
+    html: '<html style=" background: white; "><body style=" display: flex; align-items: center; justify-content: center; height: 823px; width: 823px; background: black; /* padding: 95px; */ "><p style=" color: #cccccc; font-family: SF Pro Text Regular; font-size: 35px; padding: 0 43px; letter-spacing: 0.5px; word-spacing: 1px; line-height: 50px; ">{{text}}</p></body></html>',
     content: data
 }) .then(() => console.log('The image was created successfully!'))
